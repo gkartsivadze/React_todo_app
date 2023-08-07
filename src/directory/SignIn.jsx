ce076e597878ae2be951730f37f4c5d392a6bd7a
DeNDroon <qarcigio444@mail.ru>
2023-08-07 16:29:20 +0400

    added navigation on welcome page

--- src/directory/SignIn.jsx
+++ src/directory/SignIn.jsx
@@ -1,79 +1,103 @@
 import React from "react";
-import { Navigate } from "react-router-dom"
+import { Navigate } from "react-router-dom";
 
-import { profiles } from '../users.json'
+import { profiles } from "../users.json";
 
 import { LOGIN, LOGOUT } from "../redux/actions";
 import { connect } from "react-redux";
 
 class SignIn extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            login: "",
-            password: ""
-        }
+    this.state = {
+      login: "",
+      password: "",
+      tryedLogin: false,
+    };
+  }
+
+  synchronize(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  checkData(e) {
+    e.preventDefault();
+
+    if (this.state.login.length > 2 && this.state.password.length > 2) {
+      this.compareProfiles(e);
     }
+  }
 
-    synchronize(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+  compareProfiles(e) {
+    let foundProfile = profiles?.find((x) => x.login == this.state.login);
+    if (foundProfile && this.state.password === foundProfile.password) {
+      this.props.LOGIN(foundProfile.login);
+    } else {
+      this.props.LOGOUT();
     }
-
-    checkData(e) {
-      e.preventDefault();
-
-      if(this.state.login.length > 2 && this.state.password.length > 2) {
-        this.compareProfiles(e);
-      }
+    if (this.state.tryedLogin) {
+      this.setState({ tryedLogin: true });
     }
+  }
 
-    compareProfiles(e) {
-      let foundProfile = profiles?.find(x => x.login == this.state.login);
-      if(foundProfile && this.state.password === foundProfile.password) {
-        this.props.LOGIN(foundProfile.login);
-      } else {
-        this.props.LOGOUT();
-      }
+  render() {
+    if (this.props.loginState.state) {
+      return <Navigate to="/welcome" />;
     }
-
-    render() {
-      if(this.props.loginState.state) {
-        return <Navigate to="/welcome" />
-      };
-        return (
-      <section id="form_wrapper">
-        <form id="login_form" onSubmit={this.checkData.bind(this)}>
-          <label>
-            Username
-            <input onChange={(e) => this.setState({[e.target.name]: e.target.value})} name="login" type="text" required />
-          </label>
-          <label>
-            Password
-            <input onChange={(e) => this.setState({[e.target.name]: e.target.value})} name="password" type="password" required />
-          </label>
-          {
-            this.props.loginState.state ? <p>You are logged in!</p> : <p style={{color: "red"}}>Please enter correct password</p>
-          }
-          <button>Sign in</button>
-        </form>
-      </section>
-        )
-    }
+    return (
+      <main id="login_page">
+          <form id="login_form" onSubmit={this.checkData.bind(this)}>
+            <label>
+              Username
+              <input
+                onChange={(e) =>
+                  this.setState({ [e.target.name]: e.target.value })
+                }
+                name="login"
+                type="text"
+                required
+              />
+            </label>
+            <label>
+              Password
+              <input
+                onChange={(e) =>
+                  this.setState({ [e.target.name]: e.target.value })
+                }
+                name="password"
+                type="password"
+                required
+              />
+            </label>
+            {this.state.tryedLogin ? (
+              this.props.loginState.state ? (
+                <p>You are logged in!</p>
+              ) : (
+                <p style={{ color: "red" }}>Please enter correct password</p>
+              )
+            ) : (
+              <p>Type your login and password</p>
+            )}
+            <button>Sign in</button>
+          </form>
+      </main>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
   return {
-    loginState: { state: state.current, username: state.username}
+    loginState: { state: state.current, username: state.username },
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     LOGIN: (name) => dispatch(LOGIN(name)),
-    LOGOUT: () => dispatch(LOGOUT())
+    LOGOUT: () => dispatch(LOGOUT()),
   };
 };
 
--- src/directory/Welcome.jsx
+++ src/directory/Welcome.jsx
@@ -3,13 +3,29 @@ import React from "react";
 class Welcome extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            selectedMenu: "main"
+        }
+    }
+
+    synchronizeMenu(e) {
+        this.setState({selectedMenu: e.target.value});
     }
 
     render() {
         return (
-            <>
-                <h1>Hi There</h1>
-            </>
+            <main id="welcome_page">
+                <aside id="menu_bar" onChange={this.synchronizeMenu.bind(this)}>
+                    <label>Main<input value="main" type="radio" name="selected_menu" defaultChecked /></label>
+                    <label>Staff<input value="staff" type="radio" name="selected_menu" /></label>
+                    <label>Pending<input value="pending" type="radio" name="selected_menu" /></label>
+                    <label>Log out<input value="logout" type="radio" name="selected_menu" /></label>
+                </aside>
+                { this.state.selectedMenu == "main" && <aside id="view">main</aside> }
+                { this.state.selectedMenu == "staff" && <aside id="view">staff</aside> }
+                { this.state.selectedMenu == "pending" && <aside id="view">pending</aside> }
+                { this.state.selectedMenu == "logout" && <aside id="view">log out</aside> }
+            </main>
         )
     }
 }
